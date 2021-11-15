8dbfdf1cb4692fb8ee9e16c99475453b8e6e4c91
ChrisBrown@40 <heemankamble@gmail.com>
2021-11-15 19:05:43 +0530

    Updates

---
+++ precache-manifest.d1f91d8857d566fd2116439adf39b411.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "186ed7ad63ff7b3db8e397728289cf37",
+    "url": "/Himanee-Movie_App/index.html"
+  },
+  {
+    "revision": "b2d6c8d97d0857f73ab9",
+    "url": "/Himanee-Movie_App/static/css/2.df84d89f.chunk.css"
+  },
+  {
+    "revision": "2f75f4a04aeb4fcb802a",
+    "url": "/Himanee-Movie_App/static/css/main.9f8a2250.chunk.css"
+  },
+  {
+    "revision": "b2d6c8d97d0857f73ab9",
+    "url": "/Himanee-Movie_App/static/js/2.c9cd2417.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/Himanee-Movie_App/static/js/2.c9cd2417.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2f75f4a04aeb4fcb802a",
+    "url": "/Himanee-Movie_App/static/js/main.58abb164.chunk.js"
+  },
+  {
+    "revision": "8ffcfb3c8af79806875c",
+    "url": "/Himanee-Movie_App/static/js/runtime-main.3554f7e7.js"
+  }
+]);
